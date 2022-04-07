20ab6595b43485b727f2a6066a3a70de71311926
ysfzyak <ysfzyak@gmail.com>
2022-04-07 22:33:08 +0300

    Ich habe angefangen, an dem Projekt zu arbeiten.

--- src/machinex/Main.java
+++ src/machinex/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
         
         System.out.println("Merhaba");
-        // TODO code application logic here
+        
     }
     
 }
